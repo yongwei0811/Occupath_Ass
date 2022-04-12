0c0dc377438145fb5861631bfef71b1e71971358
Denise Khew <denisekhew@gmail.com>
2022-04-13 02:41:13 +0800

    Mobile update

--- app/src/main/java/com/example/occupath/MainActivity.kt
+++ app/src/main/java/com/example/occupath/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.occupath
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.occupath.databinding.ActivityMainBinding
@@ -19,17 +21,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val bottomNav = binding.bottomNav
+        val navController = Navigation.findNavController(this,R.id.hostFragment)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        NavigationUI.setupWithNavController(bottomNav, navController)
     }
 }
